f08833991bd9c05af4b93e36387c497c9e7ff047
Iryna <iryna.burianovata@gmail.com>
2023-12-22 19:12:09 +0200

    Add new friend page

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -44,7 +44,7 @@ const Button: FC<ButtonProps> = ({
       disabled={isLoading}
       {...props}
     >
-      {isLoading ?? <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
       {children}
     </button>
   );
