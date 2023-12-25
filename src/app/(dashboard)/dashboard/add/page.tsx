9a449074dc31368abb050adbee4d8588659a5d00
Iryna <iryna.burianovata@gmail.com>
2023-12-25 17:06:39 +0200

    Change file name

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,11 +1,11 @@
-import AddFriendButton from "@/components/AddFriendButton";
+import AddFriendForm from "@/components/AddFriendForm";
 import React, { FC } from "react";
 
 const page: FC = () => {
   return (
     <main className="pt-8">
       <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
-      <AddFriendButton />
+      <AddFriendForm />
     </main>
   );
 };
